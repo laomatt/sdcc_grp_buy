99dd3589c728c88b73daecb7d47af255b59345f8
Matt L <matt@Matts-MacBook-Pro-2.local>
2018-07-05 17:59:17 -0700

    Add phone number validation for users signing up.

--- app/models/holder.rb
+++ app/models/holder.rb
@@ -12,8 +12,12 @@ class Holder < ApplicationRecord
   	# should only be able to add to max number of people
   	slot = LineDay::TimeSlot.find(line_day_time_slot_id)
   	if slot.holders.length >= 5
-  		errors.add(:user,'This shift is full, please sign up for another one.')
-  	end
+      errors.add(:user,'This shift is full, please sign up for another one.')
+    end
+
+    if user.phone.nil? || user.phone == "" || user.phone.gsub(/[^0-9,.]/, "").length < 10
+  		errors.add(:user,'You need a valid phone number (area code AND phone number) in order to sign up for a wait group.  Please click on the "Your Acct" button on the top right and enter one in the form and click the "Update Info" button. ')
+    end
   end
 
   def present_name
