edc60ae68682362d60ca2ef69aea8b23abe09674
Matt L <matt@Matts-MacBook-Pro-2.local>
2018-07-16 11:45:47 -0700

    continue re styling

--- app/javascript/components/TimeSlot.js
+++ app/javascript/components/TimeSlot.js
@@ -95,7 +95,7 @@ export default class TimeSlot extends React.Component {
 
 									    <div>
 												<div>
-										    <input type="submit" name="commit" value="leave" className="btn btn-md btn-wide btn-spec assign-btn btn-primary border-color" data-disable-with="leaving..." />
+										    <input type="submit" name="commit" value="leave" className="btn btn-md btn-wide btn-spec assign-btn btn-primary border-color" style={{backgroundColor: 'red'}} data-disable-with="leaving..." />
 												<a href="#" data-id={time.id} end-pt={"/line_day/time_slots/" + time.id} data-toggle="modal" data-target="#timeSlotEdit" className='modal-pop edit-slot btn-wide centered btn btn-spec edit-desc btn-lg btn-warning border-color' style={{'backgroundColor': 'transparent' }}>Edit Description</a>
 												</div>
 												{ 
@@ -123,7 +123,7 @@ export default class TimeSlot extends React.Component {
 											<div>
 												<div>
 													
-											    <input type="submit" name="commit" value="Join" className="btn btn-md btn-wide border-color assign-btn btn-spec btn-primary border-color" data-disable-with="joining" /><br/>
+											    <input type="submit" name="commit" value="Join" className="btn btn-md btn-wide border-color assign-btn btn-spec btn-primary border-color" style={{backgroundColor: 'green'}} data-disable-with="joining" /><br/>
 													<a href="#" data-id={time.id} end-pt={"/line_day/time_slots/" + time.id} data-toggle="modal" data-target="#timeSlotEdit" className='modal-pop edit-slot btn-wide centered btn btn-spec edit-desc btn-lg btn-warning border-color' style={{'backgroundColor': 'transparent' }}>Edit</a>
 												</div>
 												{ 
