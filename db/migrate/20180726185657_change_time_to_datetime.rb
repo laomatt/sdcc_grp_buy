95c898942d0e58ec018958a7d4ba3ed0098072d5
Matt L <matt@Matts-MacBook-Pro-2.local>
2018-07-29 19:49:10 -0700

    fix datetime migration

--- db/migrate/20180726185657_change_time_to_datetime.rb
+++ db/migrate/20180726185657_change_time_to_datetime.rb
@@ -1,7 +1,8 @@
 class ChangeTimeToDatetime < ActiveRecord::Migration[5.0]
   def change
-  	change_column :line_day_time_slots, :time, :datetime
-  	change_column :line_day_time_slots, :end_time, :datetime
-  	change_column :line_days, :start, :datetime
+  	# 'integer USING CAST(column_name AS integer)'
+  	change_column :line_day_time_slots, :time, 'datetime USING "time"::timestamp without time zone'
+  	change_column :line_day_time_slots, :end_time, 'datetime USING "end_time"::timestamp without time zone'
+  	change_column :line_days, :start, 'datetime USING "start"::timestamp without time zone'
   end
 end
