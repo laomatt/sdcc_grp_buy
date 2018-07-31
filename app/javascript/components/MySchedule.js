c9053ba14871dd96fa3ab579a24d18f2d7577440
Matt L <matt@Matts-MacBook-Pro-2.local>
2018-07-30 18:07:14 -0700

    fix schedule ui

--- app/javascript/components/MySchedule.js
+++ app/javascript/components/MySchedule.js
@@ -12,175 +12,175 @@ class MySchedule extends React.Component {
 													 		{elem.day_str}
 														</div>
 											 			<div line_day_id={elem["12:00 AM"]} className={'time_list ' + (elem["12:00 AM"] ? 'occupied' : 'free')}>
-												 			12:00 AM  { elem["12:00 AM"] ? 
-												 					<a href={'/line_days/' + elem[" 1:00 AM"]['line_day_id']}>{elem[" 1:00 AM"]['day']}</a>
+												 			 { elem["12:00 AM"] ? 
+												 					<a href={'/line_days/' + elem[" 1:00 AM"]['line_day_id']}>{elem["12:00 AM"]['day']} <br/> {elem["12:00 AM"]['present_time']}</a>
 												 					: 
-												 					''
+												 					'12:00 AM'
 												 				}
 											 			</div>
 														<div line_day_id={elem[" 1:00 AM"]} className={'time_list ' + (elem[" 1:00 AM"] ? 'occupied' : 'free')}>
-															 1:00 AM { elem[" 1:00 AM"] ? 
-															 		<a href={'/line_days/' + elem[" 1:00 AM"]['line_day_id']}>{elem[" 1:00 AM"]['day']}</a>
+															 { elem[" 1:00 AM"] ? 
+															 		<a style={ elem["12:00 AM"] ? {display: 'none'} : {display: 'block'} } href={'/line_days/' + elem[" 1:00 AM"]['line_day_id']}>{elem[" 1:00 AM"]['day']} <br/> {elem[" 1:00 AM"]['present_time']}</a>
 															 		: 
-															 		''
+															 		' 1:00 AM'
 															 	}
 														</div>
 														<div line_day_id={elem[" 2:00 AM"]} className={'time_list ' + (elem[" 2:00 AM"] ? 'occupied' : 'free')}>
-															 2:00 AM { elem[" 2:00 AM"] ? 
-															 		<a href={'/line_days/' + elem[" 2:00 AM"]['line_day_id']}>{elem[" 2:00 AM"]['day']}</a>
+															 { elem[" 2:00 AM"] ? 
+															 		<a style={ elem[" 1:00 AM"] ? {display: 'none'} : {display: 'block'} } href={'/line_days/' + elem[" 2:00 AM"]['line_day_id']}>{elem[" 2:00 AM"]['day']} <br/> {elem[" 2:00 AM"]['present_time']}</a>
 															 		: 
-															 		''
+															 		' 2:00 AM'
 															 	}
 														</div>
 														<div line_day_id={elem[" 3:00 AM"]} className={'time_list ' + (elem[" 3:00 AM"] ? 'occupied' : 'free')}>
-															 3:00 AM { elem[" 3:00 AM"] ? 
-															 		<a href={'/line_days/' + elem[" 3:00 AM"]['line_day_id']}>{elem[" 3:00 AM"]['day']}</a>
+															 { elem[" 3:00 AM"] ? 
+															 		<a style={ elem[" 2:00 AM"] ? {display: 'none'} : {display: 'block'} } href={'/line_days/' + elem[" 3:00 AM"]['line_day_id']}>{elem[" 3:00 AM"]['day']} <br/> {elem[" 3:00 AM"]['present_time']}</a>
 															 		: 
-															 		''
+															 		' 3:00 AM'
 															 	}
 														</div>
 														<div line_day_id={elem[" 4:00 AM"]} className={'time_list ' + (elem[" 4:00 AM"] ? 'occupied' : 'free')}>
-															 4:00 AM { elem[" 4:00 AM"] ? 
-															 		<a href={'/line_days/' + elem[" 4:00 AM"]['line_day_id']}>{elem[" 4:00 AM"]['day']}</a>
+															 { elem[" 4:00 AM"] ? 
+															 		<a style={ elem[" 3:00 AM"] ? {display: 'none'} : {display: 'block'} } href={'/line_days/' + elem[" 4:00 AM"]['line_day_id']}>{elem[" 4:00 AM"]['day']} <br/> {elem[" 4:00 AM"]['present_time']}</a>
 															 		: 
-															 		''
+															 		' 4:00 AM'
 															 	}
 														</div>
 														<div line_day_id={elem[" 5:00 AM"]} className={'time_list ' + (elem[" 5:00 AM"] ? 'occupied' : 'free')}>
-															 5:00 AM { elem[" 5:00 AM"] ? 
-															 		<a href={'/line_days/' + elem[" 5:00 AM"]['line_day_id']}>{elem[" 5:00 AM"]['day']}</a>
+															 { elem[" 5:00 AM"] ? 
+															 		<a style={ elem[" 4:00 AM"] ? {display: 'none'} : {display: 'block'} } href={'/line_days/' + elem[" 5:00 AM"]['line_day_id']}>{elem[" 5:00 AM"]['day']} <br/> {elem[" 5:00 AM"]['present_time']}</a>
 															 		: 
-															 		''
+															 		' 5:00 AM'
 															 	}
 														</div>
 														<div line_day_id={elem[" 6:00 AM"]} className={'time_list ' + (elem[" 6:00 AM"] ? 'occupied' : 'free')}>
-															 6:00 AM { elem[" 6:00 AM"] ? 
-															 		<a href={'/line_days/' + elem[" 6:00 AM"]['line_day_id']}>{elem[" 6:00 AM"]['day']}</a>
+															 { elem[" 6:00 AM"] ? 
+															 		<a style={ elem[" 5:00 AM"] ? {display: 'none'} : {display: 'block'} } href={'/line_days/' + elem[" 6:00 AM"]['line_day_id']}>{elem[" 6:00 AM"]['day']} <br/> {elem[" 6:00 AM"]['present_time']}</a>
 															 		: 
-															 		''
+															 		' 6:00 AM'
 															 	}
 														</div>
 														<div line_day_id={elem[" 7:00 AM"]} className={'time_list ' + (elem[" 7:00 AM"] ? 'occupied' : 'free')}>
-															 7:00 AM { elem[" 7:00 AM"] ? 
-															 		<a href={'/line_days/' + elem[" 7:00 AM"]['line_day_id']}>{elem[" 7:00 AM"]['day']}</a>
+															 { elem[" 7:00 AM"] ? 
+															 		<a style={ elem[" 6:00 AM"] ? {display: 'none'} : {display: 'block'} } href={'/line_days/' + elem[" 7:00 AM"]['line_day_id']}>{elem[" 7:00 AM"]['day']} <br/> {elem[" 7:00 AM"]['present_time']}</a>
 															 		: 
-															 		''
+															 		' 7:00 AM'
 															 	}
 														</div>
 														<div line_day_id={elem[" 8:00 AM"]} className={'time_list ' + (elem[" 8:00 AM"] ? 'occupied' : 'free')}>
-															 8:00 AM { elem[" 8:00 AM"] ? 
-															 		<a href={'/line_days/' + elem[" 8:00 AM"]['line_day_id']}>{elem[" 8:00 AM"]['day']}</a>
+															 { elem[" 8:00 AM"] ? 
+															 		<a style={ elem[" 7:00 AM"] ? {display: 'none'} : {display: 'block'} } href={'/line_days/' + elem[" 8:00 AM"]['line_day_id']}>{elem[" 8:00 AM"]['day']} <br/> {elem[" 8:00 AM"]['present_time']}</a>
 															 		: 
-															 		''
+															 		' 8:00 AM'
 															 	}
 														</div>
 														<div line_day_id={elem[" 9:00 AM"]} className={'time_list ' + (elem[" 9:00 AM"] ? 'occupied' : 'free')}>
-															 9:00 AM { elem[" 9:00 AM"] ? 
-															 		<a href={'/line_days/' + elem[" 9:00 AM"]['line_day_id']}>{elem[" 9:00 AM"]['day']}</a>
+															 { elem[" 9:00 AM"] ? 
+															 		<a style={ elem[" 8:00 AM"] ? {display: 'none'} : {display: 'block'} } href={'/line_days/' + elem[" 9:00 AM"]['line_day_id']}>{elem[" 9:00 AM"]['day']} <br/> {elem[" 9:00 AM"]['present_time']}</a>
 															 		: 
-															 		''
+															 		' 9:00 AM'
 															 	}
 														</div>
 														<div line_day_id={elem["10:00 AM"]} className={'time_list ' + (elem["10:00 AM"] ? 'occupied' : 'free')}>
-															10:00 AM { elem["10:00 AM"] ? 
-																	<a href={'/line_days/' + elem["10:00 AM"]['line_day_id']}>{elem["10:00 AM"]['day']}</a>
+															 { elem["10:00 AM"] ? 
+																	<a style={ elem[" 9:00 AM"] ? {display: 'none'} : {display: 'block'} } href={'/line_days/' + elem["10:00 AM"]['line_day_id']}>{elem["10:00 AM"]['day']} <br/> {elem["10:00 AM"]['present_time']}</a>
 																	: 
-																	''
+																	'10:00 AM'
 																}
 														</div>
 														<div line_day_id={elem["11:00 AM"]} className={'time_list ' + (elem["11:00 AM"] ? 'occupied' : 'free')}>
-															11:00 AM { elem["11:00 AM"] ? 
-																	<a href={'/line_days/' + elem["11:00 AM"]['line_day_id']}>{elem["11:00 AM"]['day']}</a>
+															 { elem["11:00 AM"] ? 
+																	<a style={ elem["10:00 AM"] ? {display: 'none'} : {display: 'block'} } href={'/line_days/' + elem["11:00 AM"]['line_day_id']}>{elem["11:00 AM"]['day']} <br/> {elem["11:00 AM"]['present_time']}</a>
 																	: 
-																	''
+																	'11:00 AM'
 																}
 														</div>
 														<div line_day_id={elem["12:00 PM"]} className={'time_list ' + (elem["12:00 PM"] ? 'occupied' : 'free')}>
-															12:00 PM { elem["12:00 PM"] ? 
-																	<a href={'/line_days/' + elem["12:00 PM"]['line_day_id']}>{elem["12:00 PM"]['day']}</a>
+															 { elem["12:00 PM"] ? 
+																	<a style={ elem["11:00 AM"] ? {display: 'none'} : {display: 'block'} } href={'/line_days/' + elem["12:00 PM"]['line_day_id']}>{elem["12:00 PM"]['day']} <br/> {elem["12:00 PM"]['present_time']}</a>
 																	: 
-																	''
+																	'12:00 PM'
 																}
 														</div>
 														<div line_day_id={elem[" 1:00 PM"]} className={'time_list ' + (elem[" 1:00 PM"] ? 'occupied' : 'free')}>
-															 1:00 PM { elem[" 1:00 PM"] ? 
-															 		<a href={'/line_days/' + elem[" 1:00 PM"]['line_day_id']}>{elem[" 1:00 PM"]['day']}</a>
+															 { elem[" 1:00 PM"] ? 
+															 		<a style={ elem["12:00 PM"] ? {display: 'none'} : {display: 'block'} } href={'/line_days/' + elem[" 1:00 PM"]['line_day_id']}>{elem[" 1:00 PM"]['day']} <br/> {elem[" 1:00 PM"]['present_time']}</a>
 															 		: 
-															 		''
+															 		' 1:00 PM'
 															 	}
 														</div>
 														<div line_day_id={elem[" 2:00 PM"]} className={'time_list ' + (elem[" 2:00 PM"] ? 'occupied' : 'free')}>
-															 2:00 PM { elem[" 2:00 PM"] ? 
-															 		<a href={'/line_days/' + elem[" 2:00 PM"]['line_day_id']}>{elem[" 2:00 PM"]['day']}</a>
+															 { elem[" 2:00 PM"] ? 
+															 		<a style={ elem[" 1:00 PM"] ? {display: 'none'} : {display: 'block'} } href={'/line_days/' + elem[" 2:00 PM"]['line_day_id']}>{elem[" 2:00 PM"]['day']} <br/> {elem[" 2:00 PM"]['present_time']}</a>
 															 		: 
-															 		''
+															 		' 2:00 PM'
 															 	}
 														</div>
 														<div line_day_id={elem[" 3:00 PM"]} className={'time_list ' + (elem[" 3:00 PM"] ? 'occupied' : 'free')}>
-															 3:00 PM { elem[" 3:00 PM"] ? 
-															 		<a href={'/line_days/' + elem[" 3:00 PM"]['line_day_id']}>{elem[" 3:00 PM"]['day']}</a>
+															 { elem[" 3:00 PM"] ? 
+															 		<a style={ elem[" 2:00 PM"] ? {display: 'none'} : {display: 'block'} } href={'/line_days/' + elem[" 3:00 PM"]['line_day_id']}>{elem[" 3:00 PM"]['day']} <br/> {elem[" 3:00 PM"]['present_time']}</a>
 															 		: 
-															 		''
+															 		' 3:00 PM'
 															 	}
 														</div>
 														<div line_day_id={elem[" 4:00 PM"]} className={'time_list ' + (elem[" 4:00 PM"] ? 'occupied' : 'free')}>
-															 4:00 PM { elem[" 4:00 PM"] ? 
-															 		<a href={'/line_days/' + elem[" 4:00 PM"]['line_day_id']}>{elem[" 4:00 PM"]['day']}</a>
+															 { elem[" 4:00 PM"] ? 
+															 		<a style={ elem[" 3:00 PM"] ? {display: 'none'} : {display: 'block'} } href={'/line_days/' + elem[" 4:00 PM"]['line_day_id']}>{elem[" 4:00 PM"]['day']} <br/> {elem[" 4:00 PM"]['present_time']}</a>
 															 		: 
-															 		''
+															 		' 4:00 PM'
 															 	}
 														</div>
 														<div line_day_id={elem[" 5:00 PM"]} className={'time_list ' + (elem[" 5:00 PM"] ? 'occupied' : 'free')}>
-															 5:00 PM { elem[" 5:00 PM"] ? 
-															 		<a href={'/line_days/' + elem[" 5:00 PM"]['line_day_id']}>{elem[" 5:00 PM"]['day']}</a>
+															 { elem[" 5:00 PM"] ? 
+															 		<a style={ elem[" 4:00 PM"] ? {display: 'none'} : {display: 'block'} } href={'/line_days/' + elem[" 5:00 PM"]['line_day_id']}>{elem[" 5:00 PM"]['day']} <br/> {elem[" 5:00 PM"]['present_time']}</a>
 															 		: 
-															 		''
+															 		' 5:00 PM'
 															 	}
 														</div>
 														<div line_day_id={elem[" 6:00 PM"]} className={'time_list ' + (elem[" 6:00 PM"] ? 'occupied' : 'free')}>
-															 6:00 PM { elem[" 6:00 PM"] ? 
-															 		<a href={'/line_days/' + elem[" 6:00 PM"]['line_day_id']}>{elem[" 6:00 PM"]['day']}</a>
+															 { elem[" 6:00 PM"] ? 
+															 		<a style={ elem[" 5:00 PM"] ? {display: 'none'} : {display: 'block'} } href={'/line_days/' + elem[" 6:00 PM"]['line_day_id']}>{elem[" 6:00 PM"]['day']} <br/> {elem[" 6:00 PM"]['present_time']}</a>
 															 		: 
-															 		''
+															 		' 6:00 PM'
 															 	}
 														</div>
 														<div line_day_id={elem[" 7:00 PM"]} className={'time_list ' + (elem[" 7:00 PM"] ? 'occupied' : 'free')}>
-															 7:00 PM { elem[" 7:00 PM"] ? 
-															 		<a href={'/line_days/' + elem[" 7:00 PM"]['line_day_id']}>{elem[" 7:00 PM"]['day']}</a>
+															 { elem[" 7:00 PM"] ? 
+															 		<a style={ elem[" 6:00 PM"] ? {display: 'none'} : {display: 'block'} } href={'/line_days/' + elem[" 7:00 PM"]['line_day_id']}>{elem[" 7:00 PM"]['day']} <br/> {elem[" 7:00 PM"]['present_time']}</a>
 															 		: 
-															 		''
+															 		' 7:00 PM'
 															 	}
 														</div>
 														<div line_day_id={elem[" 8:00 PM"]} className={'time_list ' + (elem[" 8:00 PM"] ? 'occupied' : 'free')}>
-															 8:00 PM { elem[" 8:00 PM"] ? 
-															 		<a href={'/line_days/' + elem[" 8:00 PM"]['line_day_id']}>{elem[" 8:00 PM"]['day']}</a>
+															 { elem[" 8:00 PM"] ? 
+															 		<a style={ elem[" 7:00 PM"] ? {display: 'none'} : {display: 'block'} } href={'/line_days/' + elem[" 8:00 PM"]['line_day_id']}>{elem[" 8:00 PM"]['day']} <br/> {elem[" 8:00 PM"]['present_time']}</a>
 															 		: 
-															 		''
+															 		' 8:00 PM'
 															 	}
 														</div>
 														<div line_day_id={elem[" 9:00 PM"]} className={'time_list ' + (elem[" 9:00 PM"] ? 'occupied' : 'free')}>
-															 9:00 PM { elem[" 9:00 PM"] ? 
-															 		<a href={'/line_days/' + elem[" 9:00 PM"]['line_day_id']}>{elem[" 9:00 PM"]['day']}</a>
+															 { elem[" 9:00 PM"] ? 
+															 		<a style={ elem[" 8:00 PM"] ? {display: 'none'} : {display: 'block'} } href={'/line_days/' + elem[" 9:00 PM"]['line_day_id']}>{elem[" 9:00 PM"]['day']} <br/> {elem[" 9:00 PM"]['present_time']}</a>
 															 		: 
-															 		''
+															 		' 9:00 PM'
 															 	}
 														</div>
 														<div line_day_id={elem["10:00 PM"]} className={'time_list ' + (elem["10:00 PM"] ? 'occupied' : 'free')}>
-															10:00 PM { elem["10:00 PM"] ? 
-																	<a href={'/line_days/' + elem["10:00 PM"]['line_day_id']}>{elem["10:00 PM"]['day']}</a>
+															 { elem["10:00 PM"] ? 
+																	<a style={ elem[" 9:00 PM"] ? {display: 'none'} : {display: 'block'} } href={'/line_days/' + elem["10:00 PM"]['line_day_id']}>{elem["10:00 PM"]['day']} <br/> {elem["10:00 PM"]['present_time']}</a>
 																	: 
-																	''
+																	'10:00 PM'
 																}
 														</div>
 														<div line_day_id={elem["11:00 PM"]} className={'time_list ' + (elem["11:00 PM"] ? 'occupied' : 'free')}>
-															11:00 PM { elem["11:00 PM"] ? 
-																	<a href={'/line_days/' + elem["11:00 PM"]['line_day_id']}>{elem["11:00 PM"]['day']}</a>
+															 { elem["11:00 PM"] ? 
+																	<a style={ elem["10:00 PM"] ? {display: 'none'} : {display: 'block'} } href={'/line_days/' + elem["11:00 PM"]['line_day_id']}>{elem["11:00 PM"]['day']} <br/> {elem["11:00 PM"]['present_time']}</a>
 																	: 
-																	''
+																	'11:00 PM'
 																}
 														</div>
 									</div>
-								)
+								) 
 					})}
 				</div>
     );
