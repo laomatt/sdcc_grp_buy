0abfd20e7b281466da5da1eb295a4601f9d55ab4
Matt L <matt@Matts-MacBook-Pro-2.local>
2018-07-30 20:09:00 -0700

    time conflict val

--- app/models/holder.rb
+++ app/models/holder.rb
@@ -8,6 +8,7 @@ class Holder < ApplicationRecord
   validates :user_id, uniqueness: {scope: :line_day_time_slot_id, message: 'You cannot be assigned to a group more than once.'}
 
   validate :should_only_have_a_few
+  validate :time_conflicts
 
   def should_only_have_a_few
   	# should only be able to add to max number of people
