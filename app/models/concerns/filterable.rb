0075b7f528ef270243cc567ea8e42fdf008d6b07
Matt L <matt@Matts-MacBook-Pro-2.local>
2019-05-25 08:53:48 -0700

    fix filterable agg

--- app/models/concerns/filterable.rb
+++ app/models/concerns/filterable.rb
@@ -1,6 +1,7 @@
 module Concerns::Filterable
 	extend ActiveSupport::Concern
   def filter_aggregate(filtering_params)
+  	results = ""
     filtering_params.each do |key, value|
       results += self.where("#{key} like ?", "%#{value}%")
     end
