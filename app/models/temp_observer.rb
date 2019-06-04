70fcb7f4052ed804ea34b3babd76404ab0707454
Matt L <matt@Matts-MacBook-Pro-2.local>
2019-06-03 23:06:34 -0700

    get email back up

--- app/models/temp_observer.rb
+++ app/models/temp_observer.rb
@@ -1,6 +1,6 @@
 class TempObserver < ActiveRecord::Observer
-	observe :temp
-  def after_save(request,temp)
-    Temp.send_email(request,temp)
-  end
+	# observe :temp
+ #  def after_save(request,temp)
+ #    Temp.send_email(request,temp)
+ #  end
 end
